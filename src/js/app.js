22901b85e2cb8f6831dd1fc2e31c8d61610264d0
Alexander Burachkov <zicio@yandex.ru>
2021-11-23 22:29:34 +0400

    Updated app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,8 @@ const unusedVariable = 'variable';
 
 // for demonstration purpose only
 export default function factorial(n) {
-  return (n !== 1) ? n * factorial(n - 1) : 1;
+  const result = (n !== 1) ? n * factorial(n - 1) : 1;
+  console.log(`Факториал 5 = ${result}`);
+  return result;
 }
 
-console.log(`Факториал 5 = ${factorial(5)}`);
