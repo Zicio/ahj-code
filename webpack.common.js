c3a6944271226e2290b7f3d0ea706bfe1bc6c8f3
Alexander Burachkov <zicio@yandex.ru>
2021-11-23 22:25:07 +0400

    Fixed

--- webpack.common.js
+++ webpack.common.js
@@ -25,6 +25,10 @@ module.exports = {
           },
         ],
       },
+        {
+          test: /\.(?:ico|png|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
       {
         test: /\.css$/,
         use: [
